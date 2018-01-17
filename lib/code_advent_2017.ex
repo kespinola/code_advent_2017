9c81c42e58ff15a3a355504d244172899ccd79b3
Kyle Espinola <ksespinola@gmail.com>
2018-01-16 21:41:50 -0800

    day 2 solution

--- lib/code_advent_2017.ex
+++ lib/code_advent_2017.ex
@@ -2,17 +2,4 @@ defmodule CodeAdvent do
   @moduledoc """
   Documentation for CodeAdvent.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CodeAdvent.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/code_advent_2017_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CodeAdventTest do
-  use ExUnit.Case
-  doctest CodeAdvent
-
-  test "greets the world" do
-    assert CodeAdvent.hello() == :world
-  end
-end
